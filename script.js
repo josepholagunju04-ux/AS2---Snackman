6360b37d8b072249c7c609ba43522a2fb9b84210
Joseph <josepholagunju04@gmail.com>
2026-05-13 15:01:50 +0100

    saves the scores

--- script.js
+++ script.js
@@ -175,4 +175,4 @@ document.addEventListener('keydown', movePlayer);
 drawMaze();
 startBtn.style.display = 'inline-block';
 
-let name = prompt("Enter your name");
+let name = prompt("Enter your name"); localStorage.setItem ("playerName", name);
